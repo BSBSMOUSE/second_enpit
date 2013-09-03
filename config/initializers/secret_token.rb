87e8adbc0fd2ca3a15c83808abb3fcc8c976fdad
kaneda hiroshi <kaneda-h@nifty.com>
2013-09-03 07:09:07 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecondEnpit::Application.config.secret_key_base = '31355fc038dfee6f92c65335dc26b74d1d13692e6a82d37c081af1e88a7c55ef10ee9ee84af3995d582a8bf98f15bb91d11766ffa1c2706f1824ae0cff0404cf'
